770e589b2890429e8cd70d86db714ad01c893f89
Shoaib20786 <mohd.shoaib20786@gmail.com>
2025-02-18 14:14:29 +0530

    agent scren config

--- lib/config/routes.dart
+++ lib/config/routes.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:kkp_chat_app/presentation/common/home_screen.dart';
+import 'package:kkp_chat_app/presentation/marketing/screen/agent_home_screen.dart';
 
 class Routes {
   static const String home = '/home';
+
+  //marketing side routes
+  static const String agentHomeScreen = "Agent_home_screen";
 }
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
     case Routes.home:
       return MaterialPageRoute(builder: (_) => HomeScreen());
+
+    //Marketing side routes
+    case Routes.agentHomeScreen:
+      return MaterialPageRoute(builder: (_) => AgentHomeScreen());
 
     default:
       return MaterialPageRoute(builder: (_) => HomeScreen());
---
+++ lib/presentation/marketing/screen/agent_home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AgentHomeScreen extends StatefulWidget {
+  const AgentHomeScreen({super.key});
+
+  @override
+  State<AgentHomeScreen> createState() => _AgentHomeScreenState();
+}
+
+class _AgentHomeScreenState extends State<AgentHomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
