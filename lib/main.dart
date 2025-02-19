4188445b49c24f2d6df3732849b35ddacc45de27
Shoaib20786 <mohd.shoaib20786@gmail.com>
2025-02-19 10:10:57 +0530

    main

--- lib/main.dart
+++ lib/main.dart
@@ -15,11 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'KKP Chat App',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-<<<<<<< HEAD
       initialRoute: Routes.marketingHostScreen,
-=======
-      initialRoute: Routes.onBoarding,
->>>>>>> e67a08936bc8843bf3cde2db729b778809caf76f
       onGenerateRoute: generateRoute,
     );
   }
