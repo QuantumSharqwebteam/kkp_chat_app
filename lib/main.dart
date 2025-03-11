4259c53e6ae229a5f04a3690af2696d8fd63a918
Aspire0071400 <prabhujivats@gmail.com>
2025-03-11 10:29:56 +0530

    temporary selection for customer and admin / agent added on splash screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:kkp_chat_app/config/routes/customer_routes.dart';
 import 'package:kkp_chat_app/config/routes/marketing_routes.dart';
 import 'package:kkp_chat_app/config/theme/theme.dart';
+import 'package:kkp_chat_app/presentation/common/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,8 +17,19 @@ class MyApp extends StatelessWidget {
       title: 'KKP Chat App',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      initialRoute: MarketingRoutes.marketingHostScreen,
-      onGenerateRoute: generateMarketingRoute,
+      initialRoute: "/splash",
+      routes: {
+        "/splash": (context) => Splash(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == CustomerRoutes.customerHost) {
+          return generateCustomerRoute(settings);
+        } else if (settings.name == MarketingRoutes.marketingHostScreen) {
+          return generateMarketingRoute(settings);
+        } else {
+          return null;
+        }
+      },
     );
   }
 }
---
+++ lib/presentation/common/splash.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:kkp_chat_app/config/routes/customer_routes.dart';
+import 'package:kkp_chat_app/config/routes/marketing_routes.dart';
+import 'package:kkp_chat_app/config/theme/app_colors.dart';
+import 'package:kkp_chat_app/core/utils/utils.dart';
+import 'package:kkp_chat_app/presentation/common_widgets/custom_button.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          width: Utils().width(context) * 0.8,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomButton(
+                text: 'Customer',
+                height: 50,
+                backgroundColor: AppColors.blue,
+                onPressed: () {
+                  Navigator.pushNamed(context, CustomerRoutes.customerHost);
+                },
+              ),
+              SizedBox(height: 20),
+              CustomButton(
+                text: 'Admin/Agent',
+                height: 50,
+                backgroundColor: AppColors.blue,
+                onPressed: () {
+                  Navigator.pushNamed(
+                      context, MarketingRoutes.marketingHostScreen);
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
