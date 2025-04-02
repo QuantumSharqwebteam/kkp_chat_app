903d0f9777d3e9e05438c5b10b2e2d63f385e697
Aspire0071400 <prabhujivats@gmail.com>
2025-04-02 10:57:25 +0530

    app bar todo added

--- lib/presentation/customer/widget/custom_app_bar.dart
+++ lib/presentation/customer/widget/custom_app_bar.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:kkp_chat_app/config/routes/customer_routes.dart';
 import 'package:kkp_chat_app/config/theme/app_text_styles.dart';
@@ -18,11 +19,31 @@ class _CustomAppBarState extends State<CustomAppBar> {
       onTap: () {
         // Navigator.pushNamed(context, CustomerRoutes.customerProfileSetup);
       },
-      leading: CircleAvatar(
-        radius: 26,
-        backgroundImage: AssetImage('assets/images/profile_avataar.png'),
-        foregroundImage: NetworkImage(widget.url ?? ""),
+
+      // TODO: first check for local url
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(70),
+        child: widget.url != null
+            ? CachedNetworkImage(
+                imageUrl: widget.url!,
+                width: 40,
+                height: 40,
+                fit: BoxFit.cover,
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              )
+            : Image.asset(
+                'assets/images/profile_avataar.png',
+                width: 40,
+                height: 40,
+                fit: BoxFit.cover,
+              ),
       ),
+      // CircleAvatar(
+      //   radius: 26,
+      //   backgroundImage: AssetImage('assets/images/profile_avataar.png'),
+      //   foregroundImage: NetworkImage(widget.url ?? ""),
+      // ),
       title: Text(widget.name ?? "", style: AppTextStyles.black16_500),
       subtitle:
           Text("Let's find latest messages", style: AppTextStyles.black12_400),
