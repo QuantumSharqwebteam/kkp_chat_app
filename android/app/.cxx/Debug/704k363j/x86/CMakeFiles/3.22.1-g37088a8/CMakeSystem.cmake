c608f7e760b9ae595e0e04eda32e67c456c994d8
prabhujivats@gmail.com <Aspire0071400>
2025-04-19 11:04:22 +0530

    update

--- android/app/.cxx/Debug/704k363j/x86/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
+++ android/app/.cxx/Debug/704k363j/x86/CMakeFiles/3.22.1-g37088a8/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-6.1.0-33-amd64")
+set(CMAKE_HOST_SYSTEM "Linux-6.14.0-15-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "6.1.0-33-amd64")
+set(CMAKE_HOST_SYSTEM_VERSION "6.14.0-15-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/msi/Android/Sdk/ndk/28.0.12674087/build/cmake/android.toolchain.cmake")
