680a12010cdfddf3f224f6e492f5471b79b760a4
Shoaib20786 <mohd.shoaib20786@gmail.com>
2025-06-28 15:58:42 +0530

    removed unused packages

--- lib/presentation/common_widgets/chat/chat_input_field.dart
+++ lib/presentation/common_widgets/chat/chat_input_field.dart
@@ -83,7 +83,7 @@ class _ChatInputFieldState extends State<ChatInputField>
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 23),
       child: Row(
         children: [
           IconButton(
