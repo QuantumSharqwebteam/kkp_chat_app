2a65cd2e9020c82037b74558c348a2da6f580a3c
Shoaib20786 <mohd.shoaib20786@gmail.com>
2025-06-28 12:36:42 +0530

    edeg to edge enable compact enable in mainactivity.kt file and styles.xml using latest material3 theme format and removed manage , write and read external storage policies and updated download logic for customer inquries page

--- android/app/src/main/kotlin/com/kkptextile/kkpchatapp/MainActivity.kt
+++ android/app/src/main/kotlin/com/kkptextile/kkpchatapp/MainActivity.kt
@@ -1,5 +1,24 @@
 package com.kkptextile.kkpchatapp
 
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsetsController
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Enable edge-to-edge layout
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        // Optional: make status bar text black (or white depending on background)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.setSystemBarsAppearance(
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,
+                WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS
+            )
+        }
+    }
+}
